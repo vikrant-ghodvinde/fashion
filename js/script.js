5d5b7aaab718c811d567cb3bb348ee3b9db32687
Vickys2705 <ghodvinde227@gmail.com>
2021-07-13 23:16:14 +0530

    fashion

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// navbar
+window.addEventListener('scroll', ()=>{
+  navbar = document.querySelector('nav');
+  navbar.classList.toggle('sticky', window.scrollY > 0);
+});
+
+// products
+$(document).ready(function(){
+
+  $('.list').click(function(){
+    const value = $(this).attr('data-filter');
+
+    if(value == 'all'){
+      $('.box').show('1000');
+    }
+    else {
+      $('.box').not('.'+value).hide('1000');
+      $('.box').filter('.'+value).show('1000');
+    }
+  })
+  // for active data
+  $('.list').click(function(){
+    $(this).addClass('active').siblings().removeClass('active');
+  })
+})
+
+// responsive menu
+function toggleMenu(){
+  icon = document.querySelector('.menu-icon');
+  menu = document.querySelector('.menu');
+
+  icon.classList.toggle('active');
+  menu.classList.toggle('active');
+}
